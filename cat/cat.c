60afad2b64b98e67e59fdddc90ad562e0c667588
Jacob Hicks <jacobhicks123123@gmail.com>
2017-03-20 10:56:07 -0400

    Added line numbers to cat

--- cat/cat.c
+++ cat/cat.c
@@ -6,16 +6,24 @@
 */
 
 #include <stdio.h>
+#include <string.h>
+
+char line_numbers = 0;
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
+    if(argc >= 2)
     {
-        printf("Usage: %s <file/char[]>", argv[0]);
-        return -1;
+        for(short i = 0; i < argc; i++)
+        {
+            if(strcmp(argv[i], "-l") == 0)
+            {
+                line_numbers = 1;
+            }
+        }
     }
     FILE *file = fopen(argv[1], "r");
-    int c;
+    int c, line_num = 1;
     if (file == NULL)
     {
         puts(argv[1]);
@@ -30,8 +38,19 @@ int main(int argc, char* argv[])
         c = fgetc(file);
         while(c != '\0' && c != EOF)
         {
-            putc(c, stdout);
-            c = fgetc(file);
+            if(line_numbers && c == '\n' || c == '\r')
+            {
+                putc(c, stdout);
+                printf("%d:\t", line_num++);
+                c = fgetc(file);
+            }
+            else
+            {
+                if (line_numbers && line_num == 1)
+                    printf("%d:\t", line_num++);
+                putc(c, stdout);
+                c = fgetc(file);
+            }
         }
     }
     return 0;
